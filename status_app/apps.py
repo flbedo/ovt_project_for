716c9c5e44598de05415b77a0ca0042e975c2c11
flbedo <71334111+flbedo@users.noreply.github.com>
2026-06-14 14:06:54 +0300

    Delete status_app directory

--- status_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class StatusAppConfig(AppConfig):
-    name = 'status_app'
--- status_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('index/', views.index, name='index'),
-    path('on/', views.set_on, name='set_on'),
-    path('off/', views.set_off, name='set_off'),
-]
